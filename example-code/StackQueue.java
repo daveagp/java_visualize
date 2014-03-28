b6d0996d92a00d679bf862493bf15304852a2a58
David Pritchard <daveagp@gmail.com>
2014-03-27 21:27:38 -0400

    add new example

---
+++ example-code/StackQueue.java
@@ -0,0 +1,18 @@
+public class StackQueue {
+   public static void main(String[] args) {
+      Stack<String> stack = new Stack<>();
+      Queue<String> queue = new Queue<>();
+      stack.push("first");
+      stack.push("second");
+      queue.enqueue("first");
+      queue.enqueue("second");
+      for (String s : stack) 
+          System.out.println("stack contains " + s);
+      for (String s : queue)
+          System.out.println("queue contains " + s);
+      while (!stack.isEmpty())
+          System.out.println(stack.pop());
+      while (!queue.isEmpty())
+          System.out.println(queue.dequeue());
+   }
+}
